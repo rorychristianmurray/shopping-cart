2b8919a3fd4a70e37a2028b5e6664ced5703ee56
Rory Murray <rory.christian.murray@gmail.com>
2020-01-20 16:34:31 -0500

    Create Products and components directory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
 import './App.css';
+import Products from  "./components/Products"
 
 function App() {
   return (
-    <div className="App">
-<div>Marketplace incoming</div>
+    <div className="container">
+      <h1>Marketplace Shopping Cart</h1>
+      <hr />
+      <div className="row">
+        <div className="col-md-8">
+        <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart} />
+        </div>
+        <div className="col-md-4">
+        Content 2
+        </div>
+      </div>
     </div>
   );
 }
